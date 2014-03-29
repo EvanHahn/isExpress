b62f2276afa619a5a04839c57f4ba9947c50b37a
Evan Hahn <me@evanhahn.com>
2014-03-29 18:48:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var express;
+try {
+  express = require('express');
+} catch (err) {
+}
+
+if (express) {
+
+  var http = require('http');
+  var expectedProto = express().__proto__;
+
+  module.exports = function isExpress(check) {
+
+    if (!check) {
+      return false;
+    } else if (typeof check.app !== 'function') {
+      return false;
+    } else if (check.app.__proto__ !== expectedProto) {
+      return false;
+    } else if (!(check instanceof http.IncomingMessage) &&
+             !(check instanceof http.OutgoingMessage)) {
+      return false;
+    } else {
+      return true;
+    }
+
+  };
+
+} else {
+
+  module.exports = function isExpress() {
+    return false;
+  };
+
+}
